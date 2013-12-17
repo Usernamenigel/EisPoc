25a264ad1db3baea1a272ad86bdb51319cf376d9
Usernamenigel <nigel.frangenberg@web.de>
2013-12-17 02:38:03 +0100

    Datenbank und Testclient angelegt Hinzufügen und suchen funktioniert

--- Eis_poc/src/Tester.java
+++ Eis_poc/src/Tester.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
+
+import couch.Datenverwaltung;
 
 public class Tester {
 
-	public static void main(String[] args) {
-		System.out.println("Hallo");
-		
-		System.out.println("Hallooo2");
-		
-		System.out.println("hallohierist der Laptop");
+	static Datenverwaltung dv = new Datenverwaltung();
+	static Scanner sc = new Scanner(System.in);
 
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Hallo, Git testen");
+//		Thread.sleep(10000);
+		dv.dbinfo();
+//		ausmachen();
+		hinzufuegen();
+		suche();
 	}
 
+	private static void hinzufuegen() {
+		String s1;
+		String s2;
+		System.out.println("Attribut eingeben: ");
+		s1 = sc.next();
+		System.out.println("Den Wert: ");
+		s2 = sc.next();
+		dv.eintrag(s1, s2);
+	}
+	
+	public static void suche() {
+		String s3;
+		System.out.println("Nach welchem Attribut suchen?: ");
+		s3 = sc.next();
+		dv.ausgabe(s3);
+	}
+	
+	
 }
---
+++ Eis_poc/src/couch/Datenverwaltung.java
@@ -0,0 +1,62 @@
+package couch;
+
+import java.util.List;
+
+import org.lightcouch.CouchDbClient;
+import org.lightcouch.CouchDbProperties;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+public class Datenverwaltung {
+
+	CouchDbClient dbClient2;
+	CouchDbClient dbClient3;
+	CouchDbClient dbClient = new CouchDbClient("couchdb.properties");
+
+	CouchDbProperties properties = new CouchDbProperties().setDbName("db-name")
+			.setCreateDbIfNotExist(true).setProtocol("https")
+			.setHost("example.com").setPort(443).setUsername("username")
+			.setPassword("secret").setMaxConnections(100)
+			.setConnectionTimeout(0);
+
+	public void an() {
+		// CouchDbClient dbClient = new CouchDbClient("couchdb.properties");
+		// CouchDbClient dbClient2 = new CouchDbClient("db-name", true, "http",
+		// "127.0.0.1", 5984, "username", "secret");
+		// CouchDbClient dbClient3 = new CouchDbClient(properties);
+	}
+
+	public void aus() {
+		// dbClient1.shutdown();
+		// dbClient2.shutdown();
+		// dbClient3.shutdown();
+	}
+
+	public void eintrag(String s1, String s2) {
+		String jsonstr = "{" + s1 + ":" + s2 + "}";
+		JsonObject jsonobj = dbClient.getGson().fromJson(jsonstr,JsonObject.class);
+		dbClient.save(jsonobj);
+	}
+
+	public void dbinfo() {
+		System.out.println("Die Datenbank-URI: " + dbClient.getDBUri());
+		System.out.println("Die Base-URI: " + dbClient.getBaseUri());
+	}
+
+	public void ausgabe(String s) {
+		// Holle alle Json Dokumente aus der Datenbank (view) und speichere diese in Liste
+		// Iteriere und suche nach JSON Dokumenten in der Liste
+		List<JsonObject> allDocs = dbClient.view("_all_docs").includeDocs(true).query(JsonObject.class);
+		for(JsonObject o: allDocs) {
+			//Wenn im Dokument das Attribut "s" (z.B. Alter) gefunden wird
+			if(o.has(s)) {
+				//Hole den Wert aus dem Attribut "s" und gebe aus
+				String str = o.get("Alter").toString();
+				System.out.println("JSON mit Attribut " + s + " gefunden, Wert: " + str);
+			}
+//			System.out.println(o.toString());
+		}
+	}
+
+}
